4100bddd9dbc56807403f657b96a12a975159c76
Watusyoi <asahi@yonsei.cr.kr>
2022-12-22 20:08:46 +0900

    cheange

--- final.js
+++ final.js
@@ -1,35 +1,12 @@
 
-const textTnput = document.getElementById("text_input");
+const textInput = document.getElementById("text_input");
+const todoAdd = document.getElementById("Add");
 const todoList = document.getElementById("todo_list");
 const buttonOpen = document.getElementById('modalOpen');
 const modal = document.getElementById('easyModal');
 const buttonClose = document.getElementsByClassName('modalClose')[0];
 
-
-textTnput.addEventListener("keydown", e =>{
-    const text = textTnput.value.trim();
-    
-    if (text === " " || e.key !== "Enter"){
-        return;
-    }
-
-    const li = document.createElement("li");
-    const span  = document.createElement("span");
-    const button = document.createElement("btn");
-
-    li.classList.add("list_item");
-    span.textContent = text;
-
-    button.textContent = "削除";
-    button.type ="button";
-    button.classList.add("delete-button");
-
-    li.appendChild(span);
-    li.appendChild(button);
-    todoList.appendChild(li);
-
-})
-
+//モーダルの設定
 // ボタンがクリックされた時
 buttonOpen.addEventListener('click', modalOpen);
 function modalOpen() {
@@ -49,3 +26,31 @@ function outsideClose(e) {
     modal.style.display = 'none';
   }
 }
+const addTasks = (task) => {
+  const listItem = document.createElement('li');
+  const showItem = todoList.appendChild(listItem);
+  showItem.innerHTML = task;
+
+// タスクに削除ボタンを付与
+  const deleteButton = document.createElement('button');
+  deleteButton.innerHTML = 'Delete';
+  listItem.appendChild(deleteButton);
+
+  deleteButton.addEventListener('click', evt => {
+    evt.preventDefault();
+    deleteTasks(deleteButton);
+  });
+};
+
+// 削除ボタンにタスクを消す機能を付与
+const deleteTasks = (deleteButton) => {
+  const chosenTask = deleteButton.closest('li');
+  todoList.removeChild(chosenTask);
+};
+// 追加ボタンをクリックし、イベントを発動（タスクが追加）
+todoAdd.addEventListener('click', evt => {
+  evt.preventDefault();
+  const task = textTnput.value;
+  addTasks(task);
+  textTnput.value = '';
+});
